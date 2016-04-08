64afc39a3a1c29dd6927baf8fdbc3cf83f448f8e
work <work@wei.local>
2016-04-08 10:18:04 +0800

    4/8 1017

--- lab06/lab6/assign1.c
+++ lab06/lab6/assign1.c
@@ -4,20 +4,24 @@ int main()
 {
     int num_seats, num_votes, num_lists;
     int quota, idx_list, vote, seat;
-    scanf( "Total number of seats: %d",&num_seats);
-    scanf("Total number of votes: %d",&num_votes);
-    scanf("Total number of lists: %d",&num_lists);
-    quota = num_votes / num_seats;
-    idx_list = 1;
-    while (idx_list <= num_lists) {
-        printf("Votes for list %d", idx_list);
-		scanf("%d",&vote);
-        printf("Automatic seat for list ", idx_list);
-        seat = vote / quota;
-        printf("seat: ",seat):
-        printf("Remainder for list " , idx_list );
-        printf("%d", vote - seat * quota );
-        idx_list++;
-    }
-    return 0;     
+    printf("Total number of seats: " );
+    scanf("%d",&num_seats);
+    printf("Total number of votes: ");
+        scanf("%d",&num_votes);
+    printf("Total number of lists: ");
+        scanf("%d",&num_lists);
+        quota = num_votes / num_seats;
+        idx_list = 1;
+        while (idx_list <= num_lists) {
+            printf("Votes for list %d: ", idx_list);
+    		scanf("%d",&vote);
+            printf("Automatic seat for list %d: ", idx_list);
+            seat = vote / quota;
+            printf("%d\n",seat);
+            printf("Remainder for list %d: " , idx_list );
+            printf("%d\n", vote - seat * quota );
+            idx_list++;
+        }
+    return 0;
 }
+
--- lab06/lab6/sincos.c
+++ lab06/lab6/sincos.c
@@ -1,23 +1,35 @@
+//
+//  main.c
+//  tempC
+//
+//  Created by work on 2016/4/8.
+//  Copyright © 2016年 work. All rights reserved.
+//
+
 #include <stdio.h>
+#include <math.h>
 
-#define PI 3.14159265
+#define PI 3.141592653
 
-// Task 2. Build the GetSinCos() function
+void GetSinCos(int d, double * dSin, double * dCos){
+    
+    *dSin = sin(d * PI / 180);
+    *dCos = cos(d * PI / 180);
+}
 
+int main(int argc, const char * argv[]) {
 
-int main(){
-    double dSin;
+    double dSin ;
     double dCos;
     int degree;
-
-    // Task 1. Read in user input to variable degree
-
-
-    // Task 3. Call the GetSinCos() function
-
-
+    
+    scanf("%d", &degree);
+    
+    GetSinCos(degree, &dSin, &dCos);
+    
     printf( "The sin is %g \n",dSin );
     printf( "The cos is %g \n",dCos );
     
     return 0;
 }
+
